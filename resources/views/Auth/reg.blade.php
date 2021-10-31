119559d444e6aee47bc250072ec60e842502443a
vlry <fit11box@gmail.com>
2021-10-31 14:55:44 +0400

    rmTrashModel

--- resources/views/Auth/reg.blade.php
+++ resources/views/Auth/reg.blade.php
@@ -9,17 +9,15 @@
     <form method="POST" action="{{ route('customer.registration') }}">
         @csrf
         <div class="container">
-            <div class="container col-4 input-group-md mb-5 w-25">
-                @error('email')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                @error('password')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                @error('password')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
+            @error('email')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            @error('password')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            @error('password')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
             <div class="container input-group-md mb-5 w-25">
                 <input class="form-control mb-3" type="text" name="email" placeholder="Enter your email..">
                 <input class="form-control mb-3" type="password" name="password" placeholder="Enter your password..">
